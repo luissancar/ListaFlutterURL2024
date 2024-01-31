1d7e492329e475a9c2066441bcac55670d1c0624
luis jose sanchez carrasco <luissancar@yahoo.es>
2024-01-31 10:59:04 +0100

    Final

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MiApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       // es el estilo de la app
-      title: "Primera App",  // nombre de la app
+      title: "Primera App", // nombre de la app
       home: Inicio(),
     );
   }
@@ -32,31 +32,44 @@ class _InicioState extends State<Inicio> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // estructura básica de una app
-      // estructura de una app movil
-      appBar: AppBar(
-        title: Text("Nosferatu"),
-      ),
-      body: ListView(
-        children: nosferatus.map(_buildItem).toList(),
-      ),
-    );
+        backgroundColor: Colors.blue,
+        // estructura básica de una app
+        // estructura de una app movil
+        appBar: AppBar(
+          title: Text("Nosferatu"),
+        ),
+        body: Container(
+          child: ListView(
+            children: nosferatus.map(_buildItem).toList(),
+          ),
+        ));
   }
 }
 
-
 Widget _buildItem(Nosferatu nosferatu) {
-  return new ListTile(
+  return _item(ListTile(
     title: new Text(nosferatu.name),
+    tileColor: Colors.green,
     subtitle: new Text('Subtitulo'),
     leading: new Image.network(
-     nosferatu.url, //  URL de  imagen
+      nosferatu.url, //  URL de  imagen
       width: 50.0, // Ancho de la imagen
       height: 50.0, // Altura de la imagen
       fit: BoxFit.contain, // Ajuste de la imagen dentro del contenedor
     ),
-    onTap: (){
-      print(nosferatu.name);
+    shape: RoundedRectangleBorder(
+      borderRadius:
+          BorderRadius.circular(10.0), // Ajusta el radio según sea necesario
+    ),
+    onTap: () {
+      print("Pulsado"); // Acción cuando se toca el ListTile
     },
+  ));
+}
+
+Widget _item(ListTile item) {
+  return Container(
+    child: item,
+    padding: EdgeInsets.all(16.0),
   );
 }
